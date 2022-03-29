5c6f4c58613b92b656ab6b069eca1ed02921853c
bernardocecchetto <cecchetto.bernardo@gmail.com>
2022-03-29 07:36:50 -0300

    Corrigindo o loop while

--- bot_gremio/app.py
+++ bot_gremio/app.py
@@ -3,20 +3,22 @@ from bot_gremio.credentials import *
 from datetime import datetime, timezone, timedelta
 import time
 
-data_e_hora_atuais = datetime.now()
-diferenca = timedelta(hours=-3)
-fuso_horario = timezone(diferenca)
-data_e_hora_sao_paulo = data_e_hora_atuais.astimezone(fuso_horario)
-data_e_hora_sao_paulo_em_texto = data_e_hora_sao_paulo.strftime('%H:%M')
 
 def run():
+
     while True:
+        data_e_hora_atuais = datetime.now()
+        diferenca = timedelta(hours=-3)
+        fuso_horario = timezone(diferenca)
+        data_e_hora_sao_paulo = data_e_hora_atuais.astimezone(fuso_horario)
+        data_e_hora_sao_paulo_em_texto = data_e_hora_sao_paulo.strftime('%H:%M')
+
         if data_e_hora_sao_paulo_em_texto == '19:03':
             client = tweepy.Client(BEARER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_SECRET)
             response = client.create_tweet(text='GRÊMIO')
             time.sleep(1800)
         else:
-            print(data_e_hora_sao_paulo_em_texto + ": Aguardando a hora do GREMIO")
+            print(data_e_hora_sao_paulo_em_texto + " - Aguardando a hora do GREMIO")
             time.sleep(1800)
 
 
