cdb5ddc92214f7f5b69130c7e48836f37a65f844
bernardocecchetto <cecchetto.bernardo@gmail.com>
2022-03-29 00:42:31 -0300

    Corrigindo app.py

--- bot_gremio/app.py
+++ bot_gremio/app.py
@@ -8,14 +8,19 @@ diferenca = timedelta(hours=-3)
 fuso_horario = timezone(diferenca)
 data_e_hora_sao_paulo = data_e_hora_atuais.astimezone(fuso_horario)
 data_e_hora_sao_paulo_em_texto = data_e_hora_sao_paulo.strftime('%H:%M')
-client = tweepy.Client(BEARER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_SECRET)
 
 def run():
     while True:
         if data_e_hora_sao_paulo_em_texto == '19:03':
+            client = tweepy.Client(BEARER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_SECRET)
             response = client.create_tweet(text='GRÊMIO')
+            time.sleep(1800)
         else:
             print("Aguardando a hora do GREMIO")
+            time.sleep(1800)
+
+
+run()
 
 
 
