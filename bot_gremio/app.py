454834f90c100b9d5352546369ea930b7917351f
bernardocecchetto <cecchetto.bernardo@gmail.com>
2022-03-29 00:22:47 -0300

    adicionando arquivos faltantes

--- bot_gremio/app.py
+++ bot_gremio/app.py
@@ -1,14 +1,19 @@
 import tweepy
-import schedule
 from bot_gremio.credentials import *
+from datetime import datetime, timezone, timedelta
+import time
+
+data_e_hora_atuais = datetime.now()
+diferenca = timedelta(hours=-3)
+fuso_horario = timezone(diferenca)
+data_e_hora_sao_paulo = data_e_hora_atuais.astimezone(fuso_horario)
+data_e_hora_sao_paulo_em_texto = data_e_hora_sao_paulo.strftime('%H:%M')
+client = tweepy.Client(BEARER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_SECRET)
 
 def run():
-    # Authenticate to Twitter
-    client = tweepy.Client(BEARER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_SECRET)
-
-
-    response = client.create_tweet(text='GRÊMIO')
+    while True:
+        if data_e_hora_sao_paulo_em_texto == '19:03':
+            response = client.create_tweet(text='GRÊMIO')
 
 
 
-schedule.every().day.at("19:03").do(run)
